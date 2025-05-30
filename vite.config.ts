9de2f961a9c77e751ce93fec81145444921c2feb
CthRio <3500197013@qq.com>
2025-05-30 15:30:23 +0800

    fix: 资源配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js'
+      }
+    }
   }
 })
